a1ce097f3be4d47bf88ab603258fe7f0b7fab8f2
Felipe Carvalho <felipee_sc@hotmail.com>
2021-06-11 20:11:33 -0300

    create delegate

---
+++ src/main/java/com/example/workflow/delegate/ClienteDelegate.java
@@ -0,0 +1,17 @@
+package com.example.workflow.delegate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component("helloDelegate")
+public class ClienteDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        log.info("Executando camunda");
+    }
+
+}
